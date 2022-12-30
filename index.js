84c1d312c51df498bfe715420875d415984112cb
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2022-12-30 09:36:26 +0600

    UserDataUpdate

--- index.js
+++ index.js
@@ -15,3 +15,29 @@ const uri = "mongodb+srv://IbrahimSikder:D2CbQ16MMP9U4E8j@cluster0.fomplst.mongo
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run() {
+  try {
+    const usersCollection = client.db("socialMediaApp").collection("Users");
+    const postsCollection = client.db("socialMediaApp").collection("posts");
+    const commentsCollection = client.db("socialMediaApp").collection("comments");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const details = await postsCollection.findOne(query);
+      res.send(details);
+    });
+
+    app.get("/about", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const about = await usersCollection.find(query).toArray();
+      res.send(about);
+    });
+
